94f196d45796b8b01d888575fd15b4e3bd0ee5cc
Artem Yalchik <HaveFunArtem@gmail.com>
2019-10-09 13:37:29 +0300

    Resolved: https://github.com/OMGHaveFun/OKR-List/issues/28

--- OKR-List/Classes/View Controllers/Main/NoteAddController.swift
+++ OKR-List/Classes/View Controllers/Main/NoteAddController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class NoteAddController: BaseViewController {
 
-  @IBOutlet weak var textView: UITextView!
-  
-  override func viewDidLoad() {
+    @IBOutlet weak var textView: UITextView!
+    var placeholderLabel : UILabel!
+
+    override func viewDidLoad() {
         super.viewDidLoad()
+
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "Enter some text..."
+        placeholderLabel.font = UIFont.italicSystemFont(ofSize: (textView.font?.pointSize)!)
+        placeholderLabel.sizeToFit()
+        textView.addSubview(placeholderLabel)
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: (textView.font?.pointSize)! / 2)
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.isHidden = !textView.text.isEmpty
 
     }
 
-  @IBAction func noteAddPressed(_ sender: UIButton) {
-    guard let text = textView.text else { return }
-    
-    StorageManager.shared.store(text: text)
-  }
-  
+    @IBAction func noteAddPressed(_ sender: UIButton) {
+        guard let text = textView.text else { return }
+
+        StorageManager.shared.store(text: text)
+    }
+
+}
+
+extension NoteAddController: UITextViewDelegate {
+
+    func textViewDidChange(_ textView: UITextView) {
+        placeholderLabel.isHidden = !textView.text.isEmpty
+    }
+
 }
